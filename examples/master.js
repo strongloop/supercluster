796d69550956072b10ff0687d0eb5c8370b9b212
Edmond Meinfelder <edmond@stdarg.com>
2013-09-24 16:28:55 -0700

    Integration of Master/Worker w/ discovery.

--- examples/master.js
+++ examples/master.js
@@ -2,4 +2,13 @@
 var MasterExp = require('../lib/Master');
 var Master = new MasterExp.Master();
 var data = { role: 'master' };
+
 Master.Discovery.announce('master', data, 1000, true);
+
+Master.on('workerAvailable', function(name, msg, reason) {
+  console.log('workerAvailable', name, msg, reason);
+});
+
+Master.on('workerUnavailable', function(name, msg, reason) {
+  console.log('workerUnavailable', name, msg, reason);
+});
--- examples/worker.js
+++ examples/worker.js
@@ -3,3 +3,11 @@ var WorkerExp = new require('../lib/Worker');
 var Worker = new WorkerExp.Worker();
 var data = { role: 'worker' };
 Worker.Discovery.announce('worker', data, 1000, true);
+
+Worker.on('masterAvailable', function(name, msg, reason) {
+  console.log('masterAvailable', name, msg, reason);
+});
+
+Worker.on('workerUnavailable', function(name, msg, reason) {
+  console.log('workerUnavailable', name, msg, reason);
+});
