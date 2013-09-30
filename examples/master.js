574b209b4eba335b60b3659c34a50e2198b629d9
Edmond Meinfelder <edmond@stdarg.com>
2013-09-29 19:43:39 -0700

    Bug fixes on remote task running.

--- examples/master.js
+++ examples/master.js
@@ -1,16 +1,23 @@
 'use strict';
+var util = require('util');
 var MasterExp = require('../lib/Master');
 var Master = new MasterExp.Master();
 var data = { role: 'master' };
+var debug = require('debug')('sc:example:master');
 
 Master.on('workerAvailable', function(name, worker, reason) {
-  console.log('workerAvailable', name, worker, reason);
-  var f = function() { console.log('Hello distributed world!'); };
+  debug('workerAvailable', name, worker, reason);
+  var f = function(a,b,c) { console.log('Hello %s %s!',a,b); return c; };
 
-  Master.sendTaskToWorker(worker, f, [], function(err, res, body) {
+  Master.sendTaskToWorker(worker, f, ['distributed', 'world', 'hmm'], function(err, res, body) {
+    if (err) {
+      debug('Error on sendTaskToWorker',err);
+      return;
+    }
+    debug('Work complete: ',body);
   });
 });
 
 Master.on('workerUnavailable', function(name, workerId, reason) {
-  console.log('workerUnavailable', name, workerId, reason);
+  debug('workerUnavailable', name, workerId, reason);
 });
--- examples/worker.js
+++ examples/worker.js
@@ -1,12 +1,13 @@
 'use strict';
 var WorkerExp = new require('../lib/Worker');
 var Worker = new WorkerExp.Worker();
+var debug = require('debug')('sc:example:worker');
 var data = { role: 'worker' };
 
 Worker.on('masterAvailable', function(name, msg, reason) {
-  console.log('masterAvailable', name, msg, reason);
+  debug('masterAvailable', name, msg, reason);
 });
 
 Worker.on('workerUnavailable', function(name, msg, reason) {
-  console.log('workerUnavailable', name, msg, reason);
+  debug('workerUnavailable', name, msg, reason);
 });
