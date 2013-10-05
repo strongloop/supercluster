ed9885ed36c289cb9c26169090b6dfe9903c7252
Edmond Meinfelder <edmond@stdarg.com>
2013-10-05 15:26:11 -0700

    Master now has async events for task completion.

--- examples/master.js
+++ examples/master.js
@@ -6,6 +6,10 @@ var debug = require('debug')('sc:example:master');
 var f = function(a,b,c) { console.log('Hello %s %s!',a,b); return c; };
 var args = ['distributed', 'world', 'hmm'];
 
+Master.on('workerTaskComplete', function(task, worker, result) {
+  debug('workerTaskComplete task:',task,'worker:',worker,'result:',result);
+});
+
 Master.on('workerAvailable', function(name, worker, reason) {
   debug('workerAvailable', name, worker, reason);
 
