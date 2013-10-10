e856cddb4bbc87139ccc8fad7e558e66f494c848
Edmond Meinfelder <edmond@stdarg.com>
2013-10-10 14:57:19 -0700

    Removed debugging statements.

--- examples/master.js
+++ examples/master.js
@@ -7,11 +7,11 @@ var f = function(a,b,c) { console.log('Hello %s %s!',a,b); return c; };
 var args = ['distributed', 'world', 'hmm'];
 
 Master.on('workerTaskComplete', function(task, worker, result) {
-  debug('workerTaskComplete task:',task,'worker:',worker,'result:',result);
+  //debug('workerTaskComplete task:',task,'worker:',worker,'result:',result);
 });
 
 Master.on('workerAvailable', function(name, worker, reason) {
-  debug('workerAvailable', name, worker, reason);
+  //debug('workerAvailable', name, worker, reason);
 
   Master.taskWorker(worker, f, args, function(err, res, body) {
     if (err) return debug('Error on taskWorker',err);
@@ -20,5 +20,5 @@ Master.on('workerAvailable', function(name, worker, reason) {
 });
 
 Master.on('workerUnavailable', function(name, workerId, reason) {
-  debug('workerUnavailable', name, workerId, reason);
+  //debug('workerUnavailable', name, workerId, reason);
 });
--- examples/worker.js
+++ examples/worker.js
@@ -1,14 +1,30 @@
 'use strict';
-var WorkerExp = new require('../lib/Worker');
-var Worker = new WorkerExp.Worker();
+var argv = require('optimist')
+            .default('port', 9998)
+            .default('restApiPort', 44401)
+            .argv;
 
-/*
+var WorkerExp = new require('../lib/Worker');
+var options = {
+  port: argv.port,
+  restApiPort: argv.restApiPort
+};
+
+var Worker = new WorkerExp.Worker(options);
+
 var debug = require('debug')('sc:example:worker');
 Worker.on('masterAvailable', function(name, msg, reason) {
-  debug('masterAvailable', name, msg, reason);
+  //debug('masterAvailable', name, msg, reason);
+});
+
+Worker.on('masterUnavailable', function(name, msg, reason) {
+  //debug('masterUnavailable', name, msg, reason);
+});
+
+Worker.on('workerAvailable', function(name, msg, reason) {
+  //debug('workerAvailable', name, msg, reason);
 });
 
 Worker.on('workerUnavailable', function(name, msg, reason) {
-  debug('workerUnavailable', name, msg, reason);
+  //debug('workerUnavailable', name, msg, reason);
 });
-*/
