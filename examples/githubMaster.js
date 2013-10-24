34dd69d97469febdd006f0c3409fbb35a23d1b6b
Edmond Meinfelder <edmond@stdarg.com>
2013-10-23 21:44:40 -0700

    Bug fixes for running code from github.

--- examples/githubMaster.js
+++ examples/githubMaster.js
@@ -7,21 +7,22 @@ var task = {
   type: 'github',
   user: 'strongloop',
   repo: 'slc',
-  dir: '/tmp'
+  dir: '/tmp',
   cmds: {
     pre: [
-      "npm install"
+      { cmd: 'git', args: ['checkout', 'v1.1'] },
+      { cmd: 'npm', args: ['install'] },
     ],
-    cmd: "./bin/slc help"
+    cmd: { cmd: './bin/slc', args: ['help'] }
   }
 };
 
 Master.on('workerTaskComplete', function(task, worker, result) {
-  //debug('workerTaskComplete task:',task,'worker:',worker,'result:',result);
+  debug('workerTaskComplete task:',task,'worker:',worker,'result:',result);
 });
 
 Master.on('workerAvailable', function(name, worker, reason) {
-  //debug('workerAvailable', name, worker, reason);
+  debug('workerAvailable', name, worker, reason);
 
   Master.taskWorker(worker, task, function(err, res, body) {
     if (err) return debug('Error on taskWorker',err);
@@ -29,6 +30,8 @@ Master.on('workerAvailable', function(name, worker, reason) {
   });
 });
 
+/*
 Master.on('workerUnavailable', function(name, workerId, reason) {
   //debug('workerUnavailable', name, workerId, reason);
 });
+*/
